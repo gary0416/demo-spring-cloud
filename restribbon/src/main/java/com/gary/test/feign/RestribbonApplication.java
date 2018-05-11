407d5b408c0ac306db2dc188ac7805cd8cd582f6
gary <408036296@163.com>
2018-05-11 12:51:32 +0800

    hystrix dashboard

--- restribbon/src/main/java/com/gary/test/feign/RestribbonApplication.java
+++ restribbon/src/main/java/com/gary/test/feign/RestribbonApplication.java
@@ -1,10 +1,14 @@
 package com.gary.test.feign;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +19,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
+@EnableHystrixDashboard
 public class RestribbonApplication {
 
 	public static void main(String[] args) {
@@ -27,4 +32,20 @@ public class RestribbonApplication {
 		return new RestTemplate();
 	}
 
+	/**
+	 * 配置stream
+	 * @author ztb
+	 * 2018-05-11 12:33
+	 * @return
+	 */
+	@Bean
+	public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
+	}
+	
 }
